37b5848585a7a78e5297898fd4a54cc40c3cbf07
Salman Izhar <Sbinazhar671@gmail.com>
2024-02-01 20:00:35 +0600

    DynamicId

--- src/components/DynamicId.jsx
+++ src/components/DynamicId.jsx
@@ -1,3 +1,7 @@
+// but there is a small problem: it will increase by the previous item.
+// So when your item is empty, you see a bug. so it is not recommended.
+// recommended id crypto.randomId()
+
 const getNextId = (data) => {
   const maxId = data.reduce((prev, current) =>
     prev && prev > current.id ? prev : current.id
